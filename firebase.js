c5f4dce19582222a9d18e7fb7b71aa640e4966f0
vvskchaitanya <vvskchaitanya@gmail.com>
2021-08-07 19:36:41 +0000

    initial commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBpvWzo_o1PHZRqC6Jpq4zm1ap_1rErxOE",
+    authDomain: "vvsk-arduino.firebaseapp.com",
+    databaseURL: "https://vvsk-arduino-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "vvsk-arduino",
+    storageBucket: "vvsk-arduino.appspot.com",
+    messagingSenderId: "918030025982",
+    appId: "1:918030025982:web:a7961c1dba3525933911af"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ script.js
@@ -0,0 +1,12 @@
+firebase.auth().signInWithEmailAndPassword("vvskchaitanya@gmail.com", "vvskchaitanya")
+  .then(function(result) {
+    console.log("User Successfully logged In ...",result);
+    alert("Login Success");
+  }).catch(function(error) {
+    console.error("Failed to login");
+    alert("Login Failed");
+});
+
+var defaultDatabase = firebase.database();
+var actionRef = firebase.database().ref("action");
+actionRef.set("o");
